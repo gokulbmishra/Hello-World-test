ac49fe1a5cef043ecff4a71f7fd439e53dca74cc
gokulbmishra <gokul@h2loop.ai>
2025-11-17 04:05:11 +0530

    Update call_hello_gok.py testing ls

--- call_hello_gok.py
+++ call_hello_gok.py
@@ -1,4 +1,9 @@
-# call_hello.py
+# call_hello_gok.py
 import hello_world  # Import the hello_world module
-# Call the function defined in hello_world.py
-hello_world.main()
+
+def call_main():
+    """Wrapper function to call main from hello_world.py"""
+    hello_world.main()
+
+if __name__ == "__main__":
+    call_main()
